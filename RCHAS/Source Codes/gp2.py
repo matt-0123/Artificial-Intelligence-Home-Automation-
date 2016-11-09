9fa79e44a0c194c0553e66cce8eca466a18f4693
bimodh <bimodh@gmail.com>
2016-11-09 10:19:43 -0800

    Update gp2.py

--- RCHAS/Source Codes/gp2.py
+++ RCHAS/Source Codes/gp2.py
@@ -1,11 +1,15 @@
 import os
 import subprocess
 
+#Read gpio values
 p = subprocess.Popen('cat /sys/class/gpio/gpio50/value', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
+#Check gpio status
 if (p.stdout.readline(1) == '0'): 
+	#set value to 1 if 0
 	os.system("echo 1 > /sys/class/gpio/gpio50/value")
 else:
+	#set value to 0 if 1
 	os.system("echo 0 > /sys/class/gpio/gpio50/value")
 
  
