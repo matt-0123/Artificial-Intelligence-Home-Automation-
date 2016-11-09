f9279348b3ead940c4ec13c82aa3221da6ba61ec
bimodh <bimodh@gmail.com>
2016-11-09 10:12:44 -0800

    Update execute.php

--- RCHAS/Source Codes/execute.php
+++ RCHAS/Source Codes/execute.php
@@ -1,23 +1,29 @@
 
 <?php
-
+//Start the session
 ob_start();
 session_start();
 $dir=$_SESSION['unm'];
-
+//If session valid
 if ($dir)
 {
+		//get the command
 		$command=$_POST['cmd'];
 		//echo $command;
+		//check for the first device and toggle device status
 		if ($command == 'S')
 			system("python gp1.py");
+		//check for the second device and toggle device	status
 		if ($command == 'T')
 			system("python gp2.py");
+		//check for the third device and toggle device	status
 		if ($command == 'U')
 			system("python gp3.py");	
+		//check for the fourth device and toggle device	status
 		if ($command == 'V')
 			system("python gp4.py");		
 }
+//If session invalid
 else 
 {	
 	header("Location: index.html");
