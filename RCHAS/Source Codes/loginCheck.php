1a8d2474f4dfd87ec8e3c68ab25884470fe95a25
bimodh <bimodh@gmail.com>
2016-11-09 10:15:04 -0800

    Update loginCheck.php

--- RCHAS/Source Codes/loginCheck.php
+++ RCHAS/Source Codes/loginCheck.php
@@ -1,13 +1,17 @@
 <?php
 
 ob_start();
+//Database connection info
 $hostname="localhost";
 $username="root";
 $password="root";
 $database="bimodh_rchas_db";
 
+      //Connect to database
       mysql_connect($hostname,$username,$password);
+      //Select database
       mysql_select_db($database);
+      //Get Username and password
       $myusername=$_POST['uname'];
       $mypassword=$_POST['pswrd'];
       
@@ -17,11 +21,13 @@ $database="bimodh_rchas_db";
       $mypassword = stripslashes($mypassword);
       $myusername = mysql_real_escape_string($myusername);
       $mypassword = mysql_real_escape_string($mypassword);	  
+      //Check for username and password
       $mysql = "SELECT * FROM newuserinfo WHERE loginname='$myusername' and password='$mypassword' ";      
       $result = mysql_query($mysql) or die("cannot execute query");      
       $count = mysql_num_rows($result);     	
       if($count==1)
       {
+	        //Start and register the session
 		session_start();
 		$_SESSION['unm']=$myusername;		
 		header("Location: Display.php");	
