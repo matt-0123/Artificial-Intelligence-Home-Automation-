070c5c99cbb1cb5c4950873a9d143a5e0c65e984
bimodh <bimodh@gmail.com>
2016-11-09 10:20:50 -0800

    Update gp1.py

--- RCHAS/Source Codes/gp1.py
+++ RCHAS/Source Codes/gp1.py
@@ -6,7 +6,9 @@ p = subprocess.Popen('cat /sys/class/gpio/gpio60/value', shell=True, stdout=subp
 
 #Check gpio status
 if (p.stdout.readline(1) == '0'): 
+	#set value to 1 if 0
 	os.system("echo 1 > /sys/class/gpio/gpio60/value")
 else:
+	#set value to 0 if 1
 	os.system("echo 0 > /sys/class/gpio/gpio60/value")
 
